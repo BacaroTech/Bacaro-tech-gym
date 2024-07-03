e95cfb5621bd5e9b644ac151876e3261614bab70
BacaroTech <146629169+BacaroTech@users.noreply.github.com>
2024-07-03 10:35:36 +0200

    Update lienarsearch.js

--- AlgortmiTeoria/array/search/lienarsearch.js
+++ AlgortmiTeoria/array/search/lienarsearch.js
@@ -1,3 +1,15 @@
+/*
+Questo algoritmo permette di verificare la presenza(o meno) di un elemento all'interno di un array.
+Non sfrutta nessun ragionamento sugli elementi dell'array, ma ne scorre uno ad uno e si ferma quando
+trova l'elmento che stiamo cercando
+
+Output
+0...n-1 -> posizione dell'elemento trovato
+-1 -> se l'elmento non e' presente
+
+Complessita' nel caso peggiore: O(n)
+*/
+
 function linearSearch(array, num) {
     for (let i = 0; i < array.length; i++) {
         if (array[i] === num) {
@@ -7,5 +19,5 @@ function linearSearch(array, num) {
     return -1;
 }
 
-linearSearch(numbers, 8); // returns index 4
-linearSearch(numbers, 28); // since 28 is not there, returns -1 
+linearSearch(numbers, 8); // -> 4
+linearSearch(numbers, 28); // -> -1 
