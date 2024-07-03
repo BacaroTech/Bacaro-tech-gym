9f3414fa5d77b7784e299c0293a717ae04e6c2cf
BacaroTech <146629169+BacaroTech@users.noreply.github.com>
2024-07-03 12:06:25 +0200

    Update radicsort.js

--- AlgortmiTeoria/array/sort/radicsort.js
+++ AlgortmiTeoria/array/sort/radicsort.js
@@ -1,3 +1,28 @@
+/*
+Questo algoritmo permette di mettere in ordine un array in base agli elementi che lo compongono
+visualizzazione: https://it.wikipedia.org/wiki/Radix_sort#/media/File:Radix.JPG
+
+Questo algoritmo ha una precondizione
+Le cifre dei singoli elementi si devono trovare in un intervallo tra 0 e K
+
+Questo algoritmo funziona come segue
+- vengono presi i numeri che compongono l'array
+- ognugno di essi viene ordinato dalla cifra meno significativa a quella piu' significativa
+esempio: 142, 456, 228
+- passo 1: ordino 1,4,2 -> 1,2,4 
+- passo 2: ordino 4,5,2 -> 2,4,5
+- passo 3: ordino 2,6,8 -> 2,6,8
+array finale: 142, 228, 456
+
+Output
+l'array ordinato
+
+Complessita' nel caso peggiore: O(m(n + k)), dove 
+- m sono i numeri
+- n sono le cifre
+- k sono e' il massimo dell'intervallo
+*/
+
 function digitCount(num) {
     if (num === 0) return 1
     return Math.floor(Math.log10(Math.abs(num))) + 1
