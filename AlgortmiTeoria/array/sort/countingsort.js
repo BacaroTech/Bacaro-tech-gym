b2bd8419453ce327b2a7ee6cfd3a496ec235ec1f
BacaroTech <146629169+BacaroTech@users.noreply.github.com>
2024-07-03 11:58:21 +0200

    Update countingsort.js

--- AlgortmiTeoria/array/sort/countingsort.js
+++ AlgortmiTeoria/array/sort/countingsort.js
@@ -1,4 +1,21 @@
+/*
+Questo algoritmo permette di mettere in ordine un array in base agli elementi che lo compongono
+visualizzazione: https://it.wikipedia.org/wiki/Counting_sort#/media/File:Counting_Sort_Animation.gif
 
+Questo algoritmo ha una precondizione
+Gli elmenti si devono trovare in un intervallo tra 0 e K
+
+Questo algoritmo funziona come segue
+- viene creare un array di supporto grande K posizioni chiamato "array delle occorrenze" e viene inzializzato tutto a zero
+- viene visionato l'array di partenza e ogni elemento viene preso come posizione dell'array delle occorenze => 5 -> countArray[5]++
+- viene modificato l'array delle occorenze in modo tale che al suo interno viene calcolata la somma dei prefissi per ogni indice di countArray
+- utilizzando i prefissi calcolati al passo precedente viene ordinato l'array di partenza
+
+Output
+l'array ordinato, tramite side effect
+
+Complessita' nel caso peggiore: O(n + k)
+*/
 
 function countSort(inputArray) {
     const N = inputArray.length;
@@ -22,21 +39,15 @@ function countSort(inputArray) {
         countArray[i] += countArray[i - 1];
     }
  
-    // Creating outputArray from countArray
-    const outputArray = new Array(N);
+    // Changing inputArray from countArray
     for (let i = N - 1; i >= 0; i--) {
-        outputArray[countArray[inputArray[i]] - 1] = inputArray[i];
-        countArray[inputArray[i]]--;
+        inputArray[countArray[inputArray[i]] - 1] = inputArray[i];
+        inputArray[inputArray[i]]--;
     }
  
-    return outputArray;
+    return inputArray;
 }
  
-// Driver code
+// Test sort
 const inputArray = [4, 3, 12, 1, 5, 5, 3, 9];
- 
-// Sorting the input array
-const outputArray = countSort(inputArray);
- 
-// Printing the sorted array
-console.log(outputArray.join(' '));
+console.log(countSort(inputArray););
